173da3f801e92690ecbe7f3a844181641df8413a
Animesh Shukla <oneanimesh@gmail.com>
2019-10-01 19:47:53 +0530

    Update magicset.java

--- magicset.java
+++ magicset.java
@@ -1,30 +1,58 @@
-/*package whatever //do not write package name here */
-
 import java.io.*;
 import java.util.*;
 
-class Solution {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t;
-		t = sc.nextInt();
-		while(t>0)
-		{
-		    int n,m;
-		    int c=0;
-		    n = sc.nextInt();
-		    m = sc.nextInt();
-		    
-		    int[] a = new int[n];
-		    for(int i=0;i<n;i++)
-		      {  
-		          a[i] = sc.nextInt();
-		          if(a[i]%m ==0)
-		              c++;
-		      }
-		      
-		    System.out.println((int)Math.pow(2,c)-1);
-		    t--;
-		}
-	}
+public class Main {
+    public static void main(String args[])throws IOException
+    {
+        Scanner scan = new Scanner(System.in);
+        int T = Integer.parseInt(scan.next());
+        for(int t=0;t<T;t++)
+        {
+            String s=scan.nextLine();
+            String S=scan.next(),E=scan.next();
+            int L=Integer.parseInt(scan.next()),R=Integer.parseInt(scan.next());
+            int f=-1,l=-1,c=0;
+            if(S.equals("saturday"))
+                f=0;
+            else if(S.equals("sunday"))
+                f=1;
+            else if(S.equals("monday"))
+                f=2;
+            else if(S.equals("tuesday"))
+                f=3;
+            else if(S.equals("wednesday"))
+                f=4;
+            else if(S.equals("thursday"))
+                f=5;
+            else if(S.equals("friday"))
+                f=6;
+            if(E.equals("saturday"))
+                l=0;
+            else if(E.equals("sunday"))
+                l=1;
+            else if(E.equals("monday"))
+                l=2;
+            else if(E.equals("tuesday"))
+                l=3;
+            else if(E.equals("wednesday"))
+                l=4;
+            else if(E.equals("thursday"))
+                l=5;
+            else if(E.equals("friday"))
+                l=6;
+            int x=l-f+1;
+            while(x<=R) {
+                if ((x >= L) &&( x <= R))
+                    c++;
+                x+=7;
+            }
+            if(c==0)
+                System.out.println("impossible");
+            else if(c==1)
+                System.out.println(x-7);
+            else
+                System.out.println("many");
+        }
+    }
 }
+
